fe5e6ddf72cd3e8a8c6d47677edb9cdd7661dd50
LVDScharrer <143479530+LVDScharrer@users.noreply.github.com>
2023-09-18 18:04:24 -0500

    Update field.py to include periodic phase potential

--- hoomd/md/external/field.py
+++ hoomd/md/external/field.py
@@ -82,6 +82,60 @@ class Periodic(Field):
             TypeParameterDict(i=int, A=float, w=float, p=int, len_keys=1))
         self._add_typeparam(params)
 
+class PeriodicPhase(Field):
+    """Copy of the periodic potential, but with an additional parameter allowing 
+    you to specify the initial phase of the cosine. Added by Luca Scharrer on 9/18/2023
+
+    `Periodic` computes forces and energies that induce a periodic modulation in
+    the particle concentration. The modulation is one-dimensional and extends
+    along the lattice vector :math:`\\mathbf{a}_i` of the simulation cell. This
+    force can, for example, be used to induce an ordered phase in a
+    block-copolymer melt.
+
+    The force is computed commensurate with the potential energy:
+
+    .. math::
+
+       U_i(\\vec{r_j}) = A \\tanh\\left[\\frac{1}{2 \\pi p w} \\cos\\left( phase+
+       p \\vec{b}_i\\cdot\\vec{r_j}\\right)\\right]
+
+    `Periodic` results in no virial stress due functional dependence on box
+    scaled coordinates.
+
+    .. py:attribute:: params
+
+        The `Periodic` external potential parameters. The dictionary has the
+        following keys:
+
+        * ``phase`` (`float`, **required**) - Phase of cosine at origin :math:`phase` \
+            :math:`[\\mathrm{dimensionless}]`.
+        * ``A`` (`float`, **required**) - Ordering parameter :math:`A` \
+            :math:`[\\mathrm{energy}]`.
+        * ``i`` (`int`, **required**) - :math:`\\vec{b}_i`, :math:`i=0, 1, 2`, \
+            is the simulation box's reciprocal lattice vector in the :math:`i` \
+            direction :math:`[\\mathrm{dimensionless}]`.
+        * ``w`` (`float`, **required**) - The interface width :math:`w` \
+            relative to the distance :math:`2\\pi/|\\mathbf{b_i}|` between \
+            planes in the :math:`i`-direction :math:`[\\mathrm{dimensionless}]`.
+        * ``p`` (`int`, **required**) - The periodicity :math:`p` of the \
+            modulation :math:`[\\mathrm{dimensionless}]`.
+
+        Type: `TypeParameter` [``particle_type``, `dict`]
+
+    Example::
+
+        # Apply a periodic composition modulation along the first lattice vector
+        periodic = external.field.Periodic()
+        periodic.params['A'] = dict(A=1.0, i=0, w=0.02, p=3, phase=0.0)
+        periodic.params['B'] = dict(A=-1.0, i=0, w=0.02, p=3, phase=0.0)
+    """
+    _cpp_class_name = "PotentialExternalPeriodic"
+
+    def __init__(self):
+        params = TypeParameter(
+            'params', 'particle_types',
+            TypeParameterDict(i=int, phase=float, A=float, w=float, p=int, len_keys=1))
+        self._add_typeparam(params)
 
 class Electric(Field):
     """Electric field force.
